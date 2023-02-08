39c5269c92bf3dae0b5ff6585e162a5285dd014b
imie <email@gmail.com>
2023-02-08 12:27:35 +0100

    commit file upload up

--- pub/index.php
+++ pub/index.php
@@ -18,21 +18,33 @@
     <?php
     if(isset($_POST['submit']))
     {
-    $targetDir = "img/";
-    $fileName = $_FILES['uploadedFile']['name'];
-     
-    $targetExtension = pathinfo($fileName, PATHINFO_EXTENSION);
-    $targetExtension = strtolower($targetExtension);
-    $targetFileName = $fileName . hrtime(true);
-    $targetFileName = hash("sha256", $targetFileName);
-
-
-    $targetUrl = $targetDir . $fileName . "." . $targetExtension;
-    if(file_exists($targetUrl))
-        die("Plik o tej samej nazwie już istnieje!");
-    move_uploaded_file($_FILES["uploadedFile"]["tmp_name"], $targetUrl);
-    var_dump($_FILES);
+        $targetDir = "img/";
+        $fileName = $_FILES['uploadedFile']['name'];
+        $tempFileUrl = $_FILES['uploadedFile']['tmp_name'];
+        $imageInfo = @getimagesize($tempFileUrl);
+    //var_dump($imageInfo);
+        
     
+        if(!is_array($imageInfo)){
+            die("Nieprawidłowy format obrazu");
+    }
+
+        $imgString = file_get_contents($tempFileUrl);
+        $gdImage = imagecreatefromstring($imgString);
+     
+        $targetExtension = pathinfo($fileName, PATHINFO_EXTENSION);
+        $targetExtension = strtolower($targetExtension);
+        $targetFileName = $fileName . hrtime(true);
+        $targetFileName = hash("sha256", $targetFileName);
+
+
+        $targetUrl = $targetDir . $fileName . "." . $targetExtension;
+        if(file_exists($targetUrl))
+            die("Plik o tej samej nazwie już istnieje!");
+    //move_uploaded_file($_FILES["uploadedFile"]["tmp_name"], $targetUrl);
+    //var_dump($_FILES);
+        $targetUrl = $targetDir . $targetFileName . ".webp";
+        imagewebp($gdImage, $targetUrl);
     }
 
     
